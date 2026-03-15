240f1877dd6ea476bf8dfbbabe8f91ade657efc5
Youssef Adnane <137081845+YADN2K23@users.noreply.github.com>
2026-03-15 18:57:12 +0100

    feat: ajout du repository Etudiant

---
+++ src/main/java/com/openclassrooms/etudiant/repository/EtudiantRepository.java
@@ -0,0 +1,9 @@
+package com.openclassrooms.etudiant.repository;
+
+import com.openclassrooms.etudiant.entities.Etudiant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
+}

84255f1f069904161a71a282fb992fbed3f68252
Youssef Adnane <137081845+YADN2K23@users.noreply.github.com>
2026-03-15 18:54:19 +0100

    feat: ajout de l'entité Etudiant

---
+++ src/main/java/com/openclassrooms/etudiant/entities/Etudiant.java
@@ -0,0 +1,43 @@
+package com.openclassrooms.etudiant.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "etudiant")
+public class Etudiant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @NotBlank
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+    @NotBlank
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+}
